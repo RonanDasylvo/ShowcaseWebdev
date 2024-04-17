c70f564a2bf5276873f4c55896c094c412f065d4
RonanDasylvo <ronandasylvo@outlook.com>
2024-04-17 13:15:40 +0200

    First styling of CV page

--- Showcase/tailwind.config.js
+++ Showcase/tailwind.config.js
@@ -1,7 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
     content: [
-        './Views/**/*.cshtml'
+        './Views/**/*.cshtml',
+        './wwwroot/**/*.js'
     ],
     theme: {
         extend: {},
